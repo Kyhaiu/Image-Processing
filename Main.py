a1c0cb54a340477bf47b7417b1b95d8ea97d4fe8
Bruno <37672571+sharkb8i@users.noreply.github.com>
2020-10-22 01:10:50 -0300

    Função Separação dos Canais RGB

--- Main.py
+++ Main.py
@@ -4,6 +4,9 @@ import Image  as img
 import PySimpleGUI as sg
 
 class Main:
+    # Lista de imagens
+    imgs = []
+    
     #Construtor da classe MAIN
     def __init__(self):
        pass
@@ -17,22 +20,25 @@ class Main:
 
             # Leitura das imagens
             if event == 'folderImg1':
-                self.PrepareImage(values['folderImg1'])
+                self.imgs.append(self.ReadImage(values['folderImg1']))
+                self.SeparateChannels(0)
             if event == 'folderImg2':
-                self.PrepareImage(values['folderImg2'])
+                self.imgs.append(self.ReadImage(values['folderImg2']))
+                self.SeparateChannels(1)
 
             if event == sg.WIN_CLOSED:
                 break
         window.close()
 
     # Função que faz a leitura das imagens
-    def PrepareImage(self, path):
+    def ReadImage(self, path):
         image = img.Image()
         image.setImage(path)
-        image.separate_RGB_Channels()
-        print(image.getRedChannel())    # Print do canal R de todos os pixels
-        print(image.getGreenChannel())  # Print do canal G de todos os pixels
-        print(image.getBlueChannel())   # Print do canal B de todos os pixels
+        return image
+
+    # Função que faz a separação dos canais RGB pra cada pixel
+    def SeparateChannels(self, position):
+        self.imgs[position].separate_RGB_Channels()
         
 main = Main()
 main.StartScreen()
