c2f3e1eb9d33d40613cfc0c560c71f4bd597eaa8
Marcos Augusto Campagnaro <38305986+Kyhaiu@users.noreply.github.com>
2020-12-09 17:29:18 -0300

    terminar função que identifica arquivos na pasta

--- feature-extraction/image.py
+++ feature-extraction/image.py
@@ -2,7 +2,7 @@ from PIL import Image as pil
 
 class image:
     #Construtor da classe
-    def __init__(self):
+    def __init__(self, _filename):
         pass
     
     #retorna a instancia da imagem
@@ -20,3 +20,12 @@ class image:
     #armazena o caminho + nome da imagem
     def setFilename(self, _filename):
         self.filename = _filename
+
+    #função cria uma cópia da imagem preto e banca
+    def convert_to_binary_image(self, _image, copy=True):
+        #se o parametro copia estiver como true, ele cria uma copia do objeto, caso contrario, opera o objeto por referencia
+        if copy:
+            _image = _image.copy()
+        
+        #converte a imagem para o mobo binarios
+        return _image.convert('1')
--- feature-extraction/main.py
+++ feature-extraction/main.py
@@ -1,8 +1,15 @@
+import os
 import image as img
 
 def main():
     print('Por favor informe o caminho das imagens a serem extraidas as caracteristicas: ')
     path = input()
-    print(path)
+
+    image_files = []
+
+    for name in os.listdir(path):
+        if os.path.isfile(name):
+            image_files.append(name)
+    print(image_files)
 
 main()
